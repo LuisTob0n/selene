9254575c34e334d7ba04fe085b3e54c78cc4bd08
Luis Tobon <ltob96@gmail.com>
2025-09-02 22:03:25 -0600

    Pre Alpha v0.0.3 Se cambio el forms a Microsoft2

--- js/simplyCountdown.js
+++ js/simplyCountdown.js
@@ -103,9 +103,9 @@
      */
     simplyCountdown = function (elt, args) {
         var parameters = extend({
-                year: 2015,
-                month: 6,
-                day: 28,
+                year: 2025,
+                month: 12,
+                day: 13,
                 hours: 0,
                 minutes: 0,
                 seconds: 0,
